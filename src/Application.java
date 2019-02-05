fbd4a3185fb20c8bdc443190e7d3ebb2dbe9f734
pranavpdx <pranavpdx@gmail.com>
2019-02-04 21:46:52 -0800

    Update Application.java

--- src/Application.java
+++ src/Application.java
@@ -1,16 +1,24 @@
+/*
+ * This program asks for either a file input or a console input and then either converts that input into binary form or decimal form
+ * Author: Pranav Sharma
+ * Date: 9/13/18
+ */
+
 import java.io.File;
 import java.util.Scanner;
-
 public class Application {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
+	public Application(){
 		Scanner scanner = new Scanner(System.in);
+		// asks to enter either a file or an input
 		System.out.println("Type \"file\" to enter a file or \"input\" to use the console");
 		String input = scanner.nextLine();
 		String numberInput = "";
 		String answer = "";
 		boolean keepGoing = true;
+		// checks if file, is so it reads the file and copies it to variable
+		// "numberInput", or if its a console input it copies that value to
+		// "numberInput"
 		if (input.equals("file")) {
 			try {
 				System.out.println("Enter your file name");
@@ -27,10 +35,12 @@ public class Application {
 		} else {
 			System.out.println("Invalid input");
 		}
+		// only continues program if file was found or console input is valid
 		if (keepGoing == true) {
 			System.out.println("If you would like to convert from binary to decimal type \"btd\"");
 			System.out.println("if you would like to convert from decimal to binary type \"dtb\"");
 			input = scanner.nextLine();
+			// if user chose to convert from decimal to binary then converts here
 			if (input.equals("dtb")) {
 				int number = Integer.parseInt(numberInput);
 				int power = (int) (Math.log10(number) / Math.log10(2));
@@ -43,7 +53,7 @@ public class Application {
 
 					}
 				}
-
+				// if user chose to convert from binary to decimal then converts here
 			} else if (input.equals("btd")) {
 				int number = 0;
 				int power = 0;
@@ -55,8 +65,13 @@ public class Application {
 				}
 				answer = Integer.toString(number);
 			}
-			System.out.println("The converted version is " + answer);
-			scanner.close();
+			// prints out converted version
+			System.out.println("The converted version is: " + answer);
 		}
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new Application();
 	}
 }
